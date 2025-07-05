d11245aa26d061c52601df47a65b92a3bae86050
dondreanderson <dondreanderson@gmail.com>
2025-07-05 15:01:13 -0400

    Updated filess

--- src/components/AdvancedAIFundabilityCalculator.tsx
+++ src/components/AdvancedAIFundabilityCalculator.tsx
@@ -1,4 +1,4 @@
-import { User, AssessmentData, APIResponse } from "@/types/common";
+import { User, AssessmentData, APIResponse } from "../types/common";
 'use client';
 
 import React, { useState, useEffect, useCallback } from 'react';
